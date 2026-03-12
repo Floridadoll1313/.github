1f42ae7c0ce4b11b9ea8c6599df83797959d229a
OTDAISURFERSURVIVOR <oceantidedrop@gmail.com>
2026-03-12 13:05:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-// If your hook is a .js file, use .js; if it's .jsx, use .jsx
-import { ProgressionProvider } from './source/hooks/ProgressionContext.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ProgressionProvider>
-      <App />
-    </ProgressionProvider>
-  </React.StrictMode>,
-);
+ 
+  return (
+    <Home 
+      config={myProjectConfig} 
+      lang={lang} 
+      onLangClick={handleLangToggle} 
+    />
+  );
+}
+```
